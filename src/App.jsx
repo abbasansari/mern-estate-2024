05d1771fc159437c00a3745f1995759744cdf003
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-05-08 18:06:19 +0500

    Added Pages and Routing Functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return <div className="text-red-700">App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
