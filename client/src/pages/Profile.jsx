c4197cfda5cde93d560aab93e55417ab7f3fc172
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-05-11 20:04:13 +0500

    "Add current user info to Profile page and update UI"
    This commit updates the Profile page to display the current user's information, including their email, avatar, and input fields for updating their username, email, and password. The

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-4 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">
+        {currentUser.email} Profile
+      </h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile-pic"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          className="p-3 border rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="p-3 border rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="p-3 border rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-600 uppercase text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-4">
+        <span className="text-red-600 cursor-pointer ">Delete account</span>
+
+        <span className="text-red-600 cursor-pointer ">Sign out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
