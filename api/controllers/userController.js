f8dea0057f9572c12e0c927c6095d05d03742d1d
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-05-14 11:19:10 +0500

    "Add getAlleUserListingsController to retrieve all listings of a user in userRouter and userController; add listingModel import in userController"

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import { errorHandler } from "../utils/errorHandler.js";
 import bcrypt from "bcryptjs";
+import listingModel from "./../models/listingModel.js";
 
 export const userController = (req, res) => {
   try {
@@ -69,6 +70,28 @@ export const deleteUserController = async (req, res, next) => {
       message: "User deleted",
     });
   } catch (error) {
+    next(error);
+  }
+};
+
+//getAlleUserListingsController
+
+// Controller to get all listings associated with a specific user ID
+export const getAlleUserListingsController = async (req, res, next) => {
+  // Check if the authenticated user ID matches the requested user ID
+  if (req.user.id !== req.params.id)
+    // If not authorized, return an error response
+    return next(errorHandler(401, "You can only view your own listings"));
+
+  try {
+    // Query the database for all listings associated with the user ID
+    const listings = await listingModel.find({ useRef: req.params.id });
+    // Return the listings in the response
+    return res
+      .status(200)
+      .send({ success: true, message: "ALl listings fetched", listings });
+  } catch (error) {
+    // If an error occurs during database operation, pass it to the error handling middleware
     next(error);
   }
 };
--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUserController,
+  getAlleUserListingsController,
   updateUserController,
   userController,
 } from "../controllers/userController.js";
@@ -15,5 +16,8 @@ router.post("/update/:id", verifyToken, updateUserController);
 
 // Delete User || Delete
 router.delete("/delete/:id", verifyToken, deleteUserController);
+
+// Gel All User listings || get
+router.get("/listing/:id", verifyToken, getAlleUserListingsController);
 
 export default router;
