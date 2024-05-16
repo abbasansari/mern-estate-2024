520300c7dbdb6d4201c129446872aecfb137a991
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-05-16 19:52:36 +0500

    Complete Header search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,45 +1,81 @@
-// import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      //to keep previous searchTerms we have a builtin function is js
+      const urlParams = new URLSearchParams(window.location.search);
+      urlParams.set("searchTerm", searchTerm);
+      const searchQuery = urlParams.toString();
+      navigate(`/search?${searchQuery}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
-    <nav className="flex items-center justify-between p-4 bg-slate-600 text-yellow-50">
-      <div className="flex items-center gap-44">
-        <Link to="/" className=" font-bold mr-4 text-4xl ml-12">
-          ansariEstates
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">ansari</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
-            placeholder="Search"
-            className="px-2 py-1 rounded-md text-black text-2xl focus:outline-none w-24 sm:w-64"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          {/* <FaSearch color="black" /> */}
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
+            )}
+          </Link>
+        </ul>
       </div>
-      <div className="flex items-center gap-6 mr-10">
-        <Link to="/" className="hidden sm:inline hover:underline">
-          Home
-        </Link>
-
-        <Link to="/about" className="hidden sm:inline hover:underline">
-          About
-        </Link>
-        <Link to="/profile" className="hidden sm:inline hover:underline">
-          {currentUser ? (
-            <img
-              className="rounded-full h-12 w-12 object-cover"
-              src={currentUser.avatar}
-              alt="Profile=Pic"
-            />
-          ) : (
-            <span>Signin</span>
-          )}
-        </Link>
-      </div>
-    </nav>
+    </header>
   );
 };
 
