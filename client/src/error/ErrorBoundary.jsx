1e1c8199c0ec82d9e6848409ecce88660cc6f486
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-05-16 21:27:58 +0500

    Implemented ErrorBoundary for react

---
+++ client/src/error/ErrorBoundary.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      hasError: true,
+      error: error,
+      errorInfo: errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <div className="text-center my-10 font-semibold hover:opacity-80 ">
+          <h1>Something went wrong!</h1>
+          <p>{this.state.error && this.state.error.toString()}</p>
+          <details style={{ whiteSpace: "pre-wrap" }}>
+            {this.state.errorInfo && this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
