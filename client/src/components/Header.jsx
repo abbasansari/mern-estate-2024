e501b57557b5bf2659a7eb14eb7b7d8bd452246c
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-05-17 23:00:21 +0500

    Complete HomePage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,8 +34,8 @@ const Header = () => {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">ansari</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500">abbas</span>
+            <span className="text-slate-700">Estates</span>
           </h1>
         </Link>
         <form
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="max-w-4xl mx-auto p-6">
+        <h1 className="text-3xl font-bold mb-4">About Abbas Estates</h1>
+        <p className="text-lg leading-relaxed">
+          Abbas Estates, nestled in the heart of New York, stands as a beacon of
+          unparalleled service and expertise in the realm of real estate. With a
+          rich legacy spanning years, Abbas Estates has cultivated a reputation
+          synonymous with trust, integrity, and excellence. Our commitment to
+          our clients extends beyond mere transactions; it's about forging
+          lasting relationships built on transparency and personalized
+          attention. Whether you're seeking your dream home, an investment
+          opportunity, or expert guidance in property management, Abbas Estates
+          is your steadfast partner, guiding you every step of the way. Welcome
+          to Abbas Estates, where your real estate aspirations find their
+          perfect address amidst the vibrant tapestry of New York City.
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default About;
