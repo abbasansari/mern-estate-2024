c3ced18adad790236ef9febcef903b681fa65ffa
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-05-18 11:37:29 +0500

    About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,16 +3,18 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div className="max-w-4xl mx-auto p-6">
-        <h1 className="text-3xl font-bold mb-4">About Abbas Estates</h1>
+      <div className="max-w-4xl mx-auto p-6 my-14">
+        <h1 className="text-3xl font-bold mb-4">
+          About <span className="text-slate-500">abbas</span>Estates
+        </h1>
         <p className="text-lg leading-relaxed">
           Abbas Estates, nestled in the heart of New York, stands as a beacon of
           unparalleled service and expertise in the realm of real estate. With a
           rich legacy spanning years, Abbas Estates has cultivated a reputation
           synonymous with trust, integrity, and excellence. Our commitment to
-          our clients extends beyond mere transactions; it's about forging
+          our clients extends beyond mere transactions; its about forging
           lasting relationships built on transparency and personalized
-          attention. Whether you're seeking your dream home, an investment
+          attention. Whether youre seeking your dream home, an investment
           opportunity, or expert guidance in property management, Abbas Estates
           is your steadfast partner, guiding you every step of the way. Welcome
           to Abbas Estates, where your real estate aspirations find their
